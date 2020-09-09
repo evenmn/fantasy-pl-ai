130db4c581bcc24e695033f90b4208bb900424c0
evenmn <evenmn@student.matnat.uio.no>
2020-09-09 18:54:01 +0200

    A model is now trained on data from all available seasons

--- python/create_training_set.py
+++ python/create_training_set.py
@@ -5,39 +5,41 @@ import pandas as pd
 from tqdm import tqdm
 import torch.nn as nn
 
-class TrainingSet:
-    def __init__(self, year):
-        self.year = year
-        self.collect_fixtures_data(year)
 
-    def collect_fixtures_data(self, year):
-        path = f"/home/evenmn/Fantasy-Premier-League/data/{year}/fixtures.csv"
+class TrainingSet:
+    def __init__(self, season):
+        self.season = season
+        # self.collect_fixtures_data(season)
+
+    def collect_fixtures_data(self, season):
+        path = f"/home/evenmn/Fantasy-Premier-League/data/{season}/fixtures.csv"
         self.fixtures = pd.read_csv(path)
 
-    def collect_player_ids(self, year):
-        path = f"/home/evenmn/Fantasy-Premier-League/data/{year}/player_idlist.csv"
+    def collect_player_ids(self, season):
+        path = f"/home/evenmn/Fantasy-Premier-League/data/{season}/player_idlist.csv"
         self.player_ids = pd.read_csv(path)
         return self.player_ids
 
-    def collect_player_data(self, year, id):
-        player_ids = self.collect_player_ids(year)
+    def collect_player_data(self, season, id):
+        player_ids = self.collect_player_ids(season)
         index = player_ids[player_ids["id"] == id].index[0]
         player_name = player_ids["first_name"][index] + " " + \
                       player_ids["second_name"][index]
         player_name = player_name.replace(" ", "_")
-        path = f"/home/evenmn/Fantasy-Premier-League/data/{year}/players/{player_name}_{id}/gw.csv"
+        path = f"/home/evenmn/Fantasy-Premier-League/data/{season}/players/{player_name}_{id}/gw.csv"
         return pd.read_csv(path)
 
     def expected_points(self):
         id = 55
         round = 4
-        data = self.collect_player_data(self.year, id)
+        data = self.collect_player_data(self.season, id)
         player_properties = ["bps", "creativity", "ict_index", "influence", "minutes", "opponent_team", "team_a_score", "team_h_score", "threat", "total_points", "was_home"]
         print(data[player_properties])
 
     def collect_player_datas(self):
-        year = self.year
-        path = f"/home/evenmn/Fantasy-Premier-League/data/{year}/players/"
+        print("\nCollecting player data...")
+        season = self.season
+        path = f"/home/evenmn/Fantasy-Premier-League/data/{season}/players/"
         # player_properties = ["bps", "creativity", "ict_index", "influence", "minutes", "opponent_team", "team_a_score", "team_h_score", "threat", "total_points", "was_home"]
         players = next(os.walk(path))[1]
         all_data = []
@@ -52,6 +54,7 @@ class TrainingSet:
         """
         player_properties = ["bps", "creativity", "ict_index", "influence", "minutes", "opponent_team", "team_a_score", "team_h_score", "threat", "total_points", "was_home"]
         datas = self.collect_player_datas()
+        print("\nPreparing training set...")
         inputs = []
         targets = []
         j = 0
@@ -77,32 +80,26 @@ class TrainingSet:
 
         total_size = len(inputs)
         test_size = int(test * total_size)
-        # train_size = total_size - test_size
+        train_size = total_size - test_size
 
-        test_x = inputs[:test_size]
-        test_t = targets[:test_size]
         train_x = inputs[test_size:]
-        train_t = targets[test_size:]
+        train_t = targets[test_size:].reshape(train_size, 1)
+        test_x = inputs[:test_size]
+        test_t = targets[:test_size].reshape(test_size, 1)
 
-        return test_x, test_t, train_x, train_t
+        return train_x, train_t, test_x, test_t
 
     def set_model(self, modules):
         self.model = nn.Sequential(*modules)
         return self.model
 
-    def train_torch(self, lr, max_iter):
+    def train_torch(self, x, t, lr, max_iter):
         """Train the neural network model.
         """
+        print("\nTraining...")
         # Get data
-        test_x, test_t, train_x, train_t = train.prepare_data_sets()
-        #training_data = self.generate_training_data(initial_games, goal_steps)
-        #x = torch.tensor([i[0] for i in training_data]).reshape(-1, 5)
-        #t = torch.tensor([i[1] for i in training_data]).reshape(-1, 1)
-        x = torch.tensor(train_x)
-        t = torch.tensor(train_t)
-
-        print(x.shape)
-        print(t.shape)
+        x = torch.tensor(x)
+        t = torch.tensor(t)
 
         # Define loss and optimizer
         loss_func = nn.MSELoss()
@@ -120,8 +117,42 @@ class TrainingSet:
             loss.backward()   # perform a backward pass (backpropagation)
             optimizer.step()  # update parameters
 
+    def test(self, x, t):
+        """
+        """
+        print("\nTesting...")
+        # Get data
+        x = torch.tensor(x)
+        t = torch.tensor(t)
+
+        # Test network
+        y = self.model(x.float())
+        loss_func = nn.MSELoss()
+        loss = loss_func(y, t.float())
+        print(loss)
+
+
 if __name__ == "__main__":
-    train = TrainingSet(year="2018-19")
-    modules = [nn.Linear(55, 128), nn.ReLU(), nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, 1)]
-    train.set_model(modules)
-    train.train_torch(lr=2e-2, max_iter=500)
+    seasons = ["2016-17", "2017-18", "2018-19", "2019-20"]
+    train_x, train_t, test_x, test_t = [], [], [], []
+    for season in seasons:
+        print(f"SEASON {season}")
+        train = TrainingSet(season=season)
+        modules = [nn.Linear(55, 64),
+                   nn.ReLU(),
+                   nn.Linear(64, 64),
+                   nn.ReLU(),
+                   nn.Linear(64, 1)]
+        train.set_model(modules)
+        trx, trt, tex, tet = train.prepare_data_sets()
+        train_x.append(trx)
+        train_t.append(trt)
+        test_x.append(tex)
+        test_t.append(tet)
+    train_x = np.vstack(train_x)
+    train_t = np.vstack(train_t)
+    test_x = np.vstack(test_x)
+    test_t = np.vstack(test_t)
+
+    train.train_torch(train_x, train_t, lr=2e-2, max_iter=1000)
+    train.test(test_x, test_t)
